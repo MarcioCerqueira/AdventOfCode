a4e11da44412f0996e8525d5aa5991fdd1090f93
Márcio Macedo <marciocfmacedo@hotmail.com>
2023-12-16 17:56:54 -0300

    refactor: Add starts_with to Day 4 solution

--- 2015/Day4/main.cpp
+++ 2015/Day4/main.cpp
@@ -8,7 +8,7 @@ int main(int argc, char **argv)
     std::getline(std::cin, secretKey);
 
     int number = 0;
-    while(md5((secretKey + std::to_string(number)).c_str()).substr(0, 6) != "000000")
+    while(!md5((secretKey + std::to_string(number)).c_str()).starts_with("000000"))
     {
         number++;
     }
