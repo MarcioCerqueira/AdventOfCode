12909bfd560ad34321ea0ec2487ed61b54654935
Márcio Macedo <marciocfmacedo@hotmail.com>
2023-12-17 12:41:49 -0300

    refactor: modern C++ has been added

--- 2015/Day4/main.cpp
+++ 2015/Day4/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+#include <ranges>
 #include "md5.h"
 
 int main(int argc, char **argv)
@@ -7,10 +8,6 @@ int main(int argc, char **argv)
     std::string secretKey;
     std::getline(std::cin, secretKey);
 
-    int number = 0;
-    while(!md5((secretKey + std::to_string(number)).c_str()).starts_with("000000"))
-    {
-        number++;
-    }
-    std::cout << "Answer: " << number << std::endl;
+    auto findDecimal = [&secretKey](const int number){ return md5((secretKey + std::to_string(number)).c_str()).starts_with("000000"); };
+    std::cout << "Answer: " << (std::views::iota(0) | std::views::filter(findDecimal) | std::views::take(0)).front() << std::endl;
 }
