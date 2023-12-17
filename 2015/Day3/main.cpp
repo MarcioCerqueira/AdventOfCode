7695169f5ae43774c1b070d3d17302689cc37a49
Márcio Macedo <marciocfmacedo@hotmail.com>
2023-12-17 00:56:06 -0300

    refactor: Modernize solution

--- 2015/Day3/main.cpp
+++ 2015/Day3/main.cpp
@@ -1,11 +1,21 @@
 #include <iostream>
 #include <unordered_set>
 #include <string>
+#include <algorithm>
+#include <vector>
+#include <numeric>
+#include <ranges>
 
 struct Position
 {
     int x;
     int y;
+
+    Position()
+    {
+        this->x = 0;
+        this->y = 0;
+    }
 
     Position(const int x, const int y)
     {
@@ -23,39 +33,46 @@ struct Position
         x += rhs.x;
         y += rhs.y;
     }
+
+    Position operator+(const Position& rhs)
+    {
+        return Position(x + rhs.x, y + rhs.y);
+    }
 };
 
 int main(int argc, char **argv)
 {
-    std::string directions;
-    std::cin >> directions;
+    std::string input;
+    std::cin >> input;
     
+    std::vector<Position> santaPositions, robotSantaPositions;
+    int index;
     std::unordered_set<std::string> houseIds;
-    Position santaPosition(0, 0), robotSantaPosition(0, 0), movement(0, 0);
+    
+    santaPositions.resize(input.size());
+    robotSantaPositions.resize(input.size());
 
-    houseIds.insert(santaPosition.toID());
-    bool santaMoves = true;
-    for(char direction : directions)
-    {
-        if(direction == '<') movement = Position(-1, 0);
-        else if(direction == '^') movement = Position(0, 1);
-        else if(direction == '>') movement = Position(1, 0);
-        else movement = Position(0, -1);
-        
-        if(santaMoves) 
-        {
-            santaPosition += movement;
-            houseIds.insert(santaPosition.toID());
-        }
-        else
-        {
-            robotSantaPosition += movement;
-            houseIds.insert(robotSantaPosition.toID());
-        }
-        santaMoves = !santaMoves;
-    }
+    auto transformDirection = [](const char direction) {
+        if(direction == '<') return Position(-1, 0);
+        if(direction == '^') return Position(0, 1);
+        if(direction == '>') return Position(1, 0);
+        return Position(0, -1); 
+    };
 
+    auto santaMoves = [&index](const Position p){ index++; return index % 2 == 0; };
+    auto robotSantaMoves = [&index](const Position p){ index++; return index % 2 == 1; };
+    auto insertHouseIds = [&houseIds](Position position){ houseIds.insert(position.toID()); };
+
+    index = -1;
+    auto santaDirections = input | std::ranges::views::transform(transformDirection) | std::ranges::views::filter(santaMoves);
+    std::partial_sum(santaDirections.begin(), santaDirections.end(), santaPositions.begin());
+    std::ranges::for_each(santaPositions, insertHouseIds);
+    
+    index = -1;
+    auto robotSantaDirections = input | std::ranges::views::transform(transformDirection) | std::ranges::views::filter(robotSantaMoves);
+    std::partial_sum(robotSantaDirections.begin(), robotSantaDirections.end(), robotSantaPositions.begin());
+    std::ranges::for_each(robotSantaPositions, insertHouseIds);    
+    
     std::cout << "Visited Houses: " << houseIds.size() << std::endl;
     return 0;
 }
-
